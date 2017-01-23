dda154875087ab524f2f36f4d8ae5b7f04f5d3e6
IvanPachecoEspejel <i.p.e.escuela@gmail.com>
2017-01-23 13:24:34 -0600

    Revert "Se vuelven a quitar archivos de configuracion del proyecto"
    This reverts commit 728076575c0554e23b537da799a251fb28583385.

--- app/src/main/java/hk/ust/cse/comp107x/greetfriend/ShowMessage.java
+++ app/src/main/java/hk/ust/cse/comp107x/greetfriend/ShowMessage.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ShowMessage extends AppCompatActivity {
 
     TextView txtViwMessage;
@@ -13,15 +15,11 @@ public class ShowMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_message);
-        //We get the Intent object from this activity was actived
+
         Intent in = getIntent();
-        //We get the parameter has been passed from another activity
         String message = in.getStringExtra("message");
-        //We get the UI component txtViwMessage
+
         txtViwMessage = (TextView) findViewById(R.id.txtViwMessage);
-        //We validate if the UI component exists
-        if(txtViwMessage != null){
-            txtViwMessage.setText(message);
-        }
+        txtViwMessage.setText(message);
     }
 }
